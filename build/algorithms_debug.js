f9f79b8c44f80eba0a0e34f2f7b95544eb3fd778
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-08 00:02:25 -0300

    fix find unguarded

--- build/algorithms_debug.js
+++ build/algorithms_debug.js
@@ -30,7 +30,7 @@ function __debug_find_unguarded(f, x) {
     //precondition: Exists an iterator that points to a value equal to x.
     //postcondition: [original_f, return_value) is a valid range
 
-    while ( ! source(f) != x) {
+    while (source(f) != x) {
         f = successor(f)
     }
     return f;
--- catalog/search/linear/find_unguarded.js
+++ catalog/search/linear/find_unguarded.js
@@ -2,7 +2,7 @@ function find_unguarded(f, x) {
     //precondition: Exists an iterator that points to a value equal to x.
     //postcondition: [original_f, return_value) is a valid range
 
-    while ( ! source(f) != x) {
+    while (source(f) != x) {
         f = successor(f)
     }
     return f;
